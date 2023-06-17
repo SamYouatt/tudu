777af0b31925c1d6f085f9f3e26c8cbbfe6b709a
SamYouatt <sdyouatt@btinternet.com>
2023-06-17 15:00:42 +0100

    Red: create test to create date from full input date

--- src/date.rs
+++ src/date.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 #[derive(Eq, PartialEq, Debug)]
 pub struct TuduDate {
     day: usize,
@@ -8,5 +10,25 @@ pub struct TuduDate {
 impl TuduDate {
     pub fn new(day: usize, month: usize, year: usize) -> TuduDate {
         TuduDate { day, month, year }
+    }
+
+    pub fn from_date(date: &str) -> Result<TuduDate, Box<dyn Error>> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_date_when_given_full_date_creates_correct_date() {
+        let input_date = "10-6-23";
+
+        let expected_date = TuduDate::new(10, 6, 23);
+
+        let date = TuduDate::from_date(input_date).unwrap();
+
+        assert_eq!(date, expected_date);
     }
 }
