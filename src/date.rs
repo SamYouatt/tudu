c0d4279c056c3a7d2193892ffdbacded7edbb046
SamYouatt <sdyouatt@btinternet.com>
2023-06-17 16:08:12 +0100

    create is_valid_date, returns err if not valid still rudimentary, just accepts all 29 february regardless of leap year

--- src/date.rs
+++ src/date.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use crate::error::TuduError;
 
 #[derive(Eq, PartialEq, Debug)]
 pub struct TuduDate {
@@ -15,6 +15,15 @@ impl TuduDate {
     pub fn from_date(date: &str) -> Result<TuduDate, Box<dyn Error>> {
         todo!()
     }
+}
+
+fn is_valid_date(day: usize, month: usize) -> Result<(), TuduError> {
+    return match month {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => (day <= 31).then(|| ()).ok_or(TuduError::InvalidDate),
+        4 | 6 | 9 | 11 => (day <= 30).then(|| ()).ok_or(TuduError::InvalidDate),
+        2 => (day <= 29).then(|| ()).ok_or(TuduError::InvalidDate),
+        _ => todo!(),
+    };
 }
 
 #[cfg(test)]
