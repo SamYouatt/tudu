fde3ca4d8940a09da9b7ad533ac0dc79c6f105b6
SamYouatt <sdyouatt@btinternet.com>
2023-06-18 16:32:57 +0100

    make InvalidArguments wrap String instead of &str This makes more sense anyway since the message is not a borrowed slice from anything, it belongs to the error as a way of providing a specific help message

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)]
-pub enum TuduError<'a> {
+pub enum TuduError {
     InvalidTask,
     InvalidDate,
-    InvalidArguments(&'a str),
+    InvalidArguments(String),
     UnknownCommand,
 }
