255ee7cb257ae712f774f664b29a148628e27918
SamYouatt <sdyouatt@btinternet.com>
2023-06-18 17:13:48 +0100

    remove this test, going to move it to an impl on TuduDate

--- src/storage.rs
+++ src/storage.rs
@@ -1,22 +1,4 @@
-use crate::date::TuduDate;
-
-fn filename_from_date(date: &TuduDate) -> String {
-    todo!()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::date::TuduDate;
-
-    #[test]
-    fn filename_from_date_gives_correct_name() {
-        let date = TuduDate::new(10, 6, 2023);
-
-        let expected_filename = String::from("2023-06-10.txt");
-
-        let filename = filename_from_date(&date);
-
-        assert_eq!(filename, expected_filename);
-    }
 }
