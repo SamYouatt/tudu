4562693af44e6668927c7ec4a4b967589d048230
SamYouatt <sdyouatt@btinternet.com>
2023-06-18 15:49:53 +0100

    extract all enums and structs from lib into model.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,55 +2,6 @@ use crate::date::TuduDate;
 mod date;
 mod error;
 
-#[derive(Eq, PartialEq, Debug)]
-enum Command {
-    Add(AddCommand),
-    Remove(RemoveCommand),
-    Set(SetCommand),
-    Complete(CompleteCommand),
-    View(ViewCommand),
-    Root,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-enum TaskState {
-    NotStarted,
-    Started,
-    Complete,
-    Forwarded,
-    Ignored,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct AddCommand {
-    task: String,
-    date: Option<TuduDate>,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct RemoveCommand {
-    index: usize,
-    date: Option<TuduDate>,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct SetCommand {
-    index: usize,
-    date: Option<TuduDate>,
-    state: TaskState,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct CompleteCommand {
-    index: usize,
-    date: Option<TuduDate>,
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct ViewCommand {
-    date: TuduDate,
-}
-
 fn parse_add_command(args: Vec<&str>) -> Command {
     let task_arg = args[0].to_owned();
 
---
+++ src/model.rs
@@ -0,0 +1,50 @@
+use crate::TuduDate;
+
+#[derive(Eq, PartialEq, Debug)]
+pub enum Command {
+    Add(AddCommand),
+    Remove(RemoveCommand),
+    Set(SetCommand),
+    Complete(CompleteCommand),
+    View(ViewCommand),
+    Root,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub enum TaskState {
+    NotStarted,
+    Started,
+    Complete,
+    Forwarded,
+    Ignored,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct AddCommand {
+    pub task: String,
+    pub date: Option<TuduDate>,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct RemoveCommand {
+    pub index: usize,
+    pub date: Option<TuduDate>,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct SetCommand {
+    pub index: usize,
+    pub date: Option<TuduDate>,
+    pub state: TaskState,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct CompleteCommand {
+    pub index: usize,
+    pub date: Option<TuduDate>,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct ViewCommand {
+    pub date: TuduDate,
+}
