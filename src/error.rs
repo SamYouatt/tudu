ceac0b606b18c87ae9afbaaf1c2b58db93fa4bdf
SamYouatt <sdyouatt@btinternet.com>
2023-06-20 19:46:22 +0100

    create fail_with_error to print user error message then exit with code 1

--- src/error.rs
+++ src/error.rs
@@ -12,3 +12,25 @@ pub enum TuduError {
     InvalidTaskDirectory,
     FailedToMakeDirectory,
 }
+
+pub fn fail_with_error(error: TuduError) {
+    print_user_error(error);
+
+    std::process::exit(1);
+}
+
+fn print_user_error(error: TuduError) {
+    match error {
+        TuduError::InvalidDate => todo!(),
+        TuduError::InvalidArguments(_) => todo!(),
+        TuduError::InvalidIndex => todo!(),
+        TuduError::InvalidState => todo!(),
+        TuduError::InvalidCommand => todo!(),
+        TuduError::NoTaskFile => todo!(),
+        TuduError::FailedToReadFile => todo!(),
+        TuduError::FailedToWriteFile => todo!(),
+        TuduError::BadTaskFormat => todo!(),
+        TuduError::InvalidTaskDirectory => todo!(),
+        TuduError::FailedToMakeDirectory => todo!(),
+    }
+}
